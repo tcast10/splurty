a37e17b2a26e1c7ee52ecc7929bbd6d36d185a61
Teddy <tcastro@sandiego.edu>
2019-05-13 20:45:52 +0000

    Splurty_12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do 
   root 'quotes#index'
+  resources :quotes
 end
